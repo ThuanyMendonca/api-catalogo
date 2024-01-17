1336277456fb48accb8baae2bacba34a6ced22b3
Thuany Mendonça <thuany1997@hotmail.com>
2024-01-17 13:47:22 -0300

    Add validation in class

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int Id { get; set; }
@@ -34,5 +34,22 @@ namespace APICatalogo.Models
 
         [JsonIgnore]
         public Categoria? Categoria { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if(primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra do produto deve ser maiúscula", new[] { nameof(this.Nome)});
+                }
+            }
+
+            if(this.Estoque <= 0)
+            {
+                yield return new ValidationResult("O estoque deve ser maior que zero", new[] {nameof(this.Estoque)});
+            }
+        }
     }
 }
